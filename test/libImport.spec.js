ce13c92b8fb0803e37f9eb7d9fd6caa470224fef
John Donovan <john.donovan@gtnexus.com>
2016-01-20 19:02:23 -0800

    Fixes #13

--- test/libImport.spec.js
+++ test/libImport.spec.js
@@ -3,26 +3,26 @@ let LibImport = require('../lib/import/libImport.js');
 
 describe('libImport', () => {
   it('can read a single line, single import stmnt', (done) => {
-    let scripts = ["//!import dummy\r\nvar jake = (() => {\r\n  return {\r\n    name: 'jake',\r\n    isADog: 'true'\r\n  };\r\n}());"];
+    let scripts = ["//!import dummy.js\r\nvar jake = (() => {\r\n  return {\r\n    name: 'jake',\r\n    isADog: 'true'\r\n  };\r\n}());"];
     let imports = LibImport.scan(scripts);
-    expect(imports).to.deep.equal(['dummy']);
+    expect(imports).to.deep.equal(['dummy.js']);
     expect(imports.length).to.equals(1);
     done();
   });
 
   it('can read a single line, multiple import stmnt', (done) => {
-    let scripts = ["//!import dumb, dumber,\tjeff,    jim\r\nvar jake = (() => {\r\n  return {\r\n    name: 'jake',\r\n    isADog: 'true'\r\n  };\r\n}());"];
+    let scripts = ["//!import dumb.js, dumber.js,\tjeff.js,    jim.js\r\nvar jake = (() => {\r\n  return {\r\n    name: 'jake',\r\n    isADog: 'true'\r\n  };\r\n}());"];
     let imports = LibImport.scan(scripts);
     expect(imports.length).to.equal(4);
-    expect(imports).to.deep.equal(['dumb', 'dumber', 'jeff', 'jim']);
+    expect(imports).to.deep.equal(['dumb.js', 'dumber.js', 'jeff.js', 'jim.js']);
     done();
   });
 
   it('can read a single line block import stmnt', (done) => {
-    let scripts = ["/*!import dumb, dumber,\tjeff,    jim*/\r\nvar jake = (() => {\r\n  return {\r\n    name: 'jake',\r\n    isADog: 'true'\r\n  };\r\n}());"];
+    let scripts = ["/*!import dumb.js, dumber.js,\tjeff.js,    jim.js*/\r\nvar jake = (() => {\r\n  return {\r\n    name: 'jake',\r\n    isADog: 'true'\r\n  };\r\n}());"];
     let imports = LibImport.scan(scripts);
     expect(imports.length).to.equal(4);
-    expect(imports).to.deep.equal(['dumb', 'dumber', 'jeff', 'jim']);
+    expect(imports).to.deep.equal(['dumb.js', 'dumber.js', 'jeff.js', 'jim.js']);
     done();
   });
 
