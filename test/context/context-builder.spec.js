3c80c2c1c0896e1b700ec0437a8013b8ad09ea0c
John Donovan <john.donovan@gtnexus.com>
2016-08-04 19:53:03 -0700

    Adding test stubs

---
+++ test/context/context-builder.spec.js
@@ -0,0 +1,6 @@
+/*jshint expr: true*/
+const expect = require('chai').expect;
+
+describe('ContextBuilder', () => {
+    it('can build a context');
+});
---
+++ test/context/raw-context-builder.spec.js
@@ -0,0 +1,6 @@
+/*jshint expr: true*/
+const expect = require('chai').expect;
+
+describe('RawContextBuilder', () => {
+    it('can build a context');
+});
