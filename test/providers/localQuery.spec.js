ba6dc49ba02e313936ae105af8a17acb7f5a8037
Stephen Bendfelt <stephen.bendfelt@gtnexus.com>
2016-09-09 15:26:30 -0400

    Refactor unit test to support bridging

--- test/providers/localQuery.spec.js
+++ test/providers/localQuery.spec.js
@@ -1,17 +1,18 @@
 /*jshint expr: true*/
 let expect = require('chai').expect;
 let LocalQuery = require('../../lib/providers/query/localquery');
+let Bridge = require('../../lib/providers/bridge');
 
 // this must be moved to use the factory, else bridge is not present.
-describe.skip('LocalQuery', () => {
+describe('LocalQuery', () => {
   it('handles null results', (done) => {
-      let aQuery = new LocalQuery({
+      let aQuery = new LocalQuery(new Bridge({
         store: {
           '$ParcelTrackerS1' : {
             'muppet': []
           }
         }
-      }, '$ParcelTrackerS1', 310);
+      }), '$ParcelTrackerS1', 310);
       aQuery.setOQL('snuffalupagos');
       let results = aQuery.execute();
       expect(results.hasResults).to.be.false;
