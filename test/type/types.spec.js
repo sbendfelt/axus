22909cedd7e8e90a6f62e636c0527ccc02d2d05c
John Donovan <john.donovan@gtnexus.com>
2016-11-14 16:40:40 -0800

    adding tests for type cache, splitting out v310Type

--- test/type/types.spec.js
+++ test/type/types.spec.js
@@ -3,36 +3,36 @@ const expect = require('chai').expect;
 const Types = require('../../lib/type/types');
 
 describe('Types', () => {
-  const o = {
-    __metadata: {
-      type: 'test',
-      apiVersion: '310'
-    }
-  };
-  describe('getAppXType', () => {
-    it('can read and return obj metadata', () => {
-      expect(Types.getAppXType(o)).to.equal('test');
-    });
-  });
-  describe('isAppXObject', () => {
-    it('can detmerine if _metadata.type is present', () => {
-      expect(Types.isAppXObject(o)).to.be.true;
-    });
-  });
-  describe('getTypeDefinition', () => {
-    it('returns the Type from APPX_TYPES when present', () => {
-      const order = {
+    const o = {
         __metadata: {
-          type: 'OrderDetail',
-          apiVersion: '310'
+            type: 'test',
+            apiVersion: '310'
         }
-      };
-      const OrderDetailType = Types.getTypeDefinition(order);
-      expect(OrderDetailType).to.be.ok;
-      expect(OrderDetailType.type).to.equal('OrderDetail');
-      expect(OrderDetailType.aliases).to.have.length(0);
-      expect(OrderDetailType.design).to.be.ok;
+    };
+    describe('getAppXType', () => {
+        it('can read and return obj metadata', () => {
+            expect(Types.getAppXType(o)).to.equal('test');
+        });
     });
-  });
+    describe('isAppXObject', () => {
+        it('can detmerine if _metadata.type is present', () => {
+            expect(Types.isAppXObject(o)).to.be.true;
+        });
+    });
+    describe('getTypeDefinition', () => {
+        it('returns the Type from APPX_TYPES when present', () => {
+            const order = {
+                __metadata: {
+                    type: 'OrderDetail',
+                    apiVersion: '310'
+                }
+            };
+            const OrderDetailType = Types.getTypeDefinition(order);
+            expect(OrderDetailType).to.be.ok;
+            expect(OrderDetailType.type).to.equal('OrderDetail');
+            expect(OrderDetailType.aliases).to.have.length(0);
+            expect(OrderDetailType.design).to.be.ok;
+        });
+    });
 
 });
