0ce732caef1ee9bce7cbc1552a7807f2485f60fa
John Donovan <john.donovan@gtnexus.com>
2017-06-22 14:17:53 -0700

    fixes #6

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-gulp.task('default', function() {
+gulp.task('babelify', function() {
   return gulp.src(['lib/**/*.js'])
     .pipe(babel())
     .pipe(gulp.dest('target'));
 });
+
+gulp.task('watch:lib', function() {
+  gulp.watch('lib/**/*.js', ['babelify']);
+});
+
+gulp.task('default', ['babelify']);
