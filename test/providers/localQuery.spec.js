b8c01e3781cce5b2ebddf9ed7a28a4c1e6eeb1ff
Stephen Bendfelt <stephen.bendfelt@gtnexus.com>
2019-07-22 10:06:48 -0400

    add support for addParameter to local-query-provider

--- test/providers/localQuery.spec.js
+++ test/providers/localQuery.spec.js
@@ -9,7 +9,8 @@ describe('LocalQuery', () => {
         local: {
             '$ParcelTrackerS1': {
                 'muppet': [],
-                'dozer': ['fraggle', 'rock']
+                'dozer': ['fraggle', 'rock'],
+                "gorgs.names IN ('Junior','Ma','Pa')&gorgs.height=22&gorgs.adults='true'": ['Ma.Gorg', 'Pa.Gorg']
             }
         },
         digests: []
@@ -28,6 +29,17 @@ describe('LocalQuery', () => {
         expect(results.result.length).to.equal(2);
         expect(results.result[0]).to.equal('fraggle');
         expect(results.result[1]).to.equal('rock');
+        done();
+    });
+    it('returns parameterized query results', (done) => {
+        let aQuery = new LocalQuery(bridge, '$ParcelTrackerS1', 310);
+        aQuery.setOQL("gorgs.names IN ('Junior','Ma','Pa')");
+        aQuery.addParameter('gorgs.height', 22);
+        aQuery.addParameter('gorgs.adults', "'true'");
+        let results = aQuery.execute();
+        expect(results.result.length).to.equal(2);
+        expect(results.result[0]).to.equal('Ma.Gorg');
+        expect(results.result[1]).to.equal('Pa.Gorg');
         done();
     });
 });
