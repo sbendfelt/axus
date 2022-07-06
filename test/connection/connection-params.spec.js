d2fa1749abdb9b78434c297e9c4942529939ef21
Andrew Reynolds <andrew.reynolds@infor.com>
2022-07-06 18:53:02 -0400

    fixup! asking for url and dataKey

--- test/connection/connection-params.spec.js
+++ test/connection/connection-params.spec.js
@@ -7,16 +7,16 @@ describe('ConnectionParams', () => {
         it('does not throw on correct params obj', () => {
             const params = {
                 username: 'John',
-                password: '432bf30cec437eca88c2c356ee77dfdcd042ea0d',
-                dataKey: '2ab257c0355d75294f54cd40b8aa224bc569eac3',
+                password: '...',
+                dataKey: '<datakey>',
                 url: 'https://commerce-demo.gtnexus.com/rest/'
             };
             ConnectionParams.validate(params);
         });
         it('throws error when params is missing value', () => {
           const params = {
-              password: '432bf30cec437eca88c2c356ee77dfdcd042ea0d',
-              dataKey: '2ab257c0355d75294f54cd40b8aa224bc569eac3',
+              password: '...',
+              dataKey: '<datakey>',
               url: 'https://commerce-demo.gtnexus.com/rest/'
           };
           const validFn = ConnectionParams.validate.bind(null, params);
